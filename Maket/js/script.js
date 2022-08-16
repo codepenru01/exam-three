fc15002d10d4a6178b600c82517ac80f0d706b4c
Ahadov Jahongir Sobirjonovich <103167390+codepenru01@users.noreply.github.com>
2022-08-16 10:41:49 +0100

    exam three

--- Maket/js/script.js
+++ Maket/js/script.js
@@ -1,66 +1,66 @@
-let elSiteSelect = document.querySelector(".form__select");
-let elInputRadio = document.querySelector(".form__input-radio");
-let elInputCheckOn = document.querySelector(".form__input-check-on");
-let elInputCheckExtra = document.querySelector(".form__input-check-extra");
+let inwebsite = document.querySelector(".form__select");
+let labelRadio = document.querySelector(".form__input-radio");
+let labelheck = document.querySelector(".form__input-check-on");
+let labelAppen = document.querySelector(".form__input-check-extra");
 
-let elInfoSpan = document.querySelector(".info__pizza-bread");
-let elInfoSpanFirst = document.querySelector(".info__pizza-size");
-let elInfoSpanSecond = document.querySelector(".info__pizza-on-food");
-let elInfoSpanThird = document.querySelector(".info__pizza-on-extra");
-
-
-
+let spanInfo = document.querySelector(".info__pizza-bread");
+let spanFirst = document.querySelector(".info__pizza-size");
+let spanSecond = document.querySelector(".info__pizza-on-food");
+let spanThird = document.querySelector(".info__pizza-on-extra");
 
 let selectArr = ["Yupqa" , "O`rtacha", "Qalin"];
-const ChooseArr = [25, 30, 35,];
-let chooseOnFood = ["Pomidor" , "Kurka Go`shti" , "Zaytun", "Tuzlangan bodring", "Qo`ziqorin", "Qazi"];
-let chooseOnExtra = ["Achchiq", "Sosikali", "Mozarella",];
+const chooseArr = [25, 30, 35,];
+let onFood = ["Pomidor" , "Kurka Go`shti" , "Zaytun", "Tuzlangan bodring", "Qo`ziqorin", "Qazi"];
+let onExtra = ["Achchiq", "Sosikali", "Mozarella",];
 let targetArr = [];
 
-for (let extra = 0; extra < chooseOnExtra.length; extra++) {
+for (let extra = 0; extra < onExtra.length; extra++) {
   let inputExtra = document.createElement("input");
   inputExtra.setAttribute("type", "checkbox");
   inputExtra.setAttribute("name" , "food_on_extra_pizza");
   inputExtra.setAttribute("class", "me-1");
   let labelExtra = document.createElement("label");
-  labelExtra.setAttribute("class", "me-4")
+  labelExtra.setAttribute("class", "me-4");
   
-  inputExtra.value = chooseOnExtra[extra] ;
-  labelExtra.textContent = chooseOnExtra[extra] ;
+  inputExtra.value = onExtra[extra] ;
+  labelExtra.textContent = onExtra[extra] ;
   labelExtra.prepend(inputExtra);
-  elInputCheckExtra.appendChild(labelExtra);
+  labelAppen.appendChild(labelExtra);
   
   let extraItem = document.createElement("li");
   inputExtra.addEventListener("click",function() {
     if(inputExtra.checked) {
-      targetArr.push(chooseOnExtra[extra]);
-      extraItem.textContent = chooseOnExtra[extra];
+      targetArr.push(onExtra[extra]);
+      spanThird.appendChild(extraItem);
+      extraItem.textContent = onExtra[extra];
     } else{ 
-      elInfoSpanThird.removeChild(extraItem);
+      spanThird.removeChild(extraItem);
     }
   })
 }
 
-for (let food = 0; food < chooseOnFood.length; food++) {
+for (let food = 0; food < onFood.length; food++) {
   let inputFood = document.createElement("input");
   inputFood.setAttribute("type", "checkbox");
   inputFood.setAttribute("name" , "food_on_pizza");
   inputFood.setAttribute("class", "me-1" , "checkbox");
   let labelFood = document.createElement("label");
-  labelFood.setAttribute("class", "me-4")
+  labelFood.setAttribute("class", "me-4");
   
-  inputFood.value = chooseOnFood[food];
-  labelFood.textContent = chooseOnFood[food] ;
+  inputFood.value = onFood[food];
+  labelFood.textContent = onFood[food] ;
   labelFood.prepend(inputFood);
-  elInputCheckOn.appendChild(labelFood);
+  labelheck.appendChild(labelFood);
   
   
   let foodItem = document.createElement("li");
   inputFood.addEventListener("click",function() {
     if(inputFood.checked) {
-      targetArr.push(chooseOnFood[food]);
-      foodItem.textContent = chooseOnFood[food];
+      targetArr.push(onFood[food]);
+      spanSecond.appendChild(foodItem);
+      foodItem.textContent = onFood[food];
     } else{ 
+      spanSecond.removeChild(foodItem);
     }
   })
 }
@@ -69,26 +69,27 @@ for(let element = 0; element < selectArr.length; element++) {
   let selectOpt = document.createElement("option");
   selectOpt.value = selectArr[element];
   selectOpt.textContent = selectArr[element];
-  elSiteSelect.appendChild(selectOpt);
+  inwebsite.appendChild(selectOpt);
   
-  elSiteSelect.addEventListener('change', function(e){
+  inwebsite.addEventListener('change', function(e){
+    spanInfo.textContent = e.target.value
   })
 }
 
-for (let choose = 0; choose < ChooseArr.length; choose++) {
-  let inputChoose = document.createElement("input");
-  inputChoose.setAttribute("type", "radio");
-  inputChoose.setAttribute("name" , "Size_pizza");
-  inputChoose.setAttribute("class", "me-1");
+for (let choose = 0; choose < chooseArr.length; choose++) {
+  let inputCars = document.createElement("input");
+  inputCars.setAttribute("type", "radio");
+  inputCars.setAttribute("name" , "Size_pizza");
+  inputCars.setAttribute("class", "me-1");
   let labelChoose = document.createElement("label");
   labelChoose.setAttribute("class", "me-4")
   
-  inputChoose.value = `${ChooseArr[choose]} sm`;
-  labelChoose.textContent = `${ChooseArr[choose]} sm`;
-  labelChoose.prepend(inputChoose);
-  elInputRadio.appendChild(labelChoose);
+  inputCars.value = `${ChooseArr[choose]} sm`;
+  labelChoose.textContent = `${chooseArr[choose]} sm`;
+  labelChoose.prepend(inputCars);
+  labelRadio.appendChild(labelChoose);
   
-  elInputRadio.addEventListener('change', function(e){
-    elInfoSpanFirst.textContent = e.target.value
+  labelRadio.addEventListener('change', function(e){
+    spanFirst.textContent = e.target.value;
   })
 }
